0a1af7d9e8e5818554bea756b320d7232bc5bf38
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-09-01 16:40:39 +0330

    add logic in js

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function longestWord(){
+    const sentence = document.getElementById("sentence").value;
+    console.log(sentence);
+    const words = sentence.split(' ');
+    console.log(words);
+    // loop for ignoring the punctuation
+    for (let i = 0; i < words.length; i++){
+        if (words[i].includes('!')){
+            words[i] = words[i].replaceAll("!", "");
+            console.log(words[i]);
+        }
+        if (words[i].includes("@")){
+            words[i] = words[i].replaceAll('@', '');
+            console.log(words[i]);
+        }
+        if (words[i].includes('&')){
+            words[i] = words[i].replaceAll('&', '');
+            console.log(words[i]);
+        }
+    }
+
+    let longestWordofSentence = words[0];
+
+    for (let i = 0; i < words.length; i++) {
+        let wordLen = words[i].length;
+        if (wordLen > longestWordofSentence.length){
+            longestWordofSentence = words[i];
+        }
+    }
+    console.log("Longest word is: " + longestWordofSentence);
+}
