27ff02e88c39e53bdd4d5d709ed6a36cca7538e1
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-09-02 15:51:13 +0330

    add some changes in the ignoring loop

--- scripts.js
+++ scripts.js
@@ -1,30 +1,20 @@
 function longestWord(){
-    const sentence = document.getElementById("sentence").value;
+    let sentence = document.getElementById("sentence").value;
     console.log(sentence);
-    const words = sentence.split(' ');
-    console.log(words);
-    // loop for ignoring the punctuation
-    for (let i = 0; i < words.length; i++){
-        if (words[i].includes('!')){
-            words[i] = words[i].replaceAll("!", "");
-            console.log(words[i]);
-        }
-        if (words[i].includes("@")){
-            words[i] = words[i].replaceAll('@', '');
-            console.log(words[i]);
-        }
-        if (words[i].includes('&')){
-            words[i] = words[i].replaceAll('&', '');
-            console.log(words[i]);
-        }
-    }
 
-    let longestWordofSentence = words[0];
+    const newArr = [];
+    const words = sentence.split(' ').forEach(element => {
+        newArr.push(element.replace(/\W/g, ''));
+    });
 
-    for (let i = 0; i < words.length; i++) {
-        let wordLen = words[i].length;
+    console.log(newArr);
+
+    let longestWordofSentence = newArr[0];
+
+    for (let i = 0; i < newArr.length; i++) {
+        let wordLen = newArr[i].length;
         if (wordLen > longestWordofSentence.length){
-            longestWordofSentence = words[i];
+            longestWordofSentence = newArr[i];
         }
     }
     console.log("Longest word is: " + longestWordofSentence);
